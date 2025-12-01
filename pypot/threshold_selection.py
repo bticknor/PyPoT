b8b183cc2b8efff05f42920d334c5fc062ae7abb
Benjamin Ticknor <ticknorbenjamin@gmail.com>
2025-12-01 07:51:12 -0800

    clamp imputed p vals to avoid runtimewarning downstream

--- pypot/threshold_selection.py
+++ pypot/threshold_selection.py
@@ -113,8 +113,11 @@ def forward_stop_adjusted_p(p_vals):
     returns:
         (int): location in p_vals array of chosen u threshold
     """
+
     # imputation procedure occaisonally yields p > 1
-    p_vals = np.minimum(p_vals, 1)
+    # clamp down to 1 - eps to avoid log(0) runtimewarning
+    eps = np.finfo(float).eps  # ~2e-16
+    p_vals = np.minimum(p_vals, 1 - eps)
 
     k = np.arange(len(p_vals)) + 1
     scaled_qsums = -1 / k * np.cumsum(np.log(1 - p_vals))
